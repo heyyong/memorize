131cc0296be2e743a08f10bf80ebc9653dcfeb3a
heyyong <43958460+heyyong@users.noreply.github.com>
2022-12-06 04:38:21 +0000

    feat: add vocabulary entity

--- src/entity/vocabularies.ts
+++ src/entity/vocabularies.ts
@@ -1,22 +1,44 @@
-import { Entity, Column, PrimaryColumn, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm'
+import { Column, CreateDateColumn, Entity, Index, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
+// export enum Level {
+//     NULL,
+//     A1,
+//     A2,
+//     B1,
+//     B2,
+//     C1,
+//     C2,
+// }
+
+export enum PartOfSpeech {
+    Adjective,
+    Nouns,
+    Verbs,
+    Adverb,
+    Conjunction,
+    Determiner,
+    Number,
+    OrdinalNumber,
+    Preposition,
+    Predeterminer,
+    Pronoun,
+    Prefix,
+    Suffix,
+    Exclamation,
+}
 
 @Entity()
-export class Voc {
+export class MVocabulary {
     @PrimaryGeneratedColumn()
     public id: number;
 
     @Index()
-    @Column({ unique: true, })
-    public voc: string;
+    @Column()
+    public spelling: string;
 
     @Index()
     @Column()
-    public originDict: string;
-
-    @Index()
-    @Column()
-    public sync: boolean;
+    public dict: string;
 
     @CreateDateColumn()
     public created: number;
@@ -25,55 +47,21 @@ export class Voc {
     public updated: number;
 }
 
-export enum VocProperty {
-    'verb' = 'verb',
-    'noun' = 'noun',
-    'conj' = 'conj',
-    'prep' = 'prep',
-    'adj' = 'adj',
-    'adv' = 'adv',
-    'pron' = 'pron',
-    'article' = 'article',
-}
+
 
 @Entity()
-export class Property {
+export class MPartOfSpeech {
     @PrimaryGeneratedColumn()
     public id: number;
 
     @Index()
     @Column()
-    public voc_id: number;
+    @ManyToOne(() => MVocabulary, (voc) => voc.id)
+    public vocId: number;
 
     @Index()
     @Column()
-    public prop: VocProperty;
-
-    @CreateDateColumn()
-    public created: number;
-
-    @UpdateDateColumn()
-    public updated: number;
-}
-
-@Entity()
-export class Pronunciation {
-    @PrimaryGeneratedColumn()
-    public id: number;
-
-    @Index()
-    @Column()
-    public voc_id: number;
-
-    @Index()
-    @Column()
-    public prop_id: number;
-
-    @Column()
-    public audio: string;
-
-    @Column()
-    public phonetic: string;
+    public pos: string;
 
     @CreateDateColumn()
     public created: number;
@@ -83,20 +71,23 @@ export class Pronunciation {
 }
 
 @Entity()
-export class Meaning {
+export class MSense {
     @PrimaryGeneratedColumn()
     public id: number;
 
     @Index()
     @Column()
-    public voc: string;
-
-    @Column()
-    public content: string;
+    @ManyToOne(() => MPartOfSpeech, (pos) => pos.id)
+    public posId: number;
 
     @Index()
     @Column()
-    public propId: number;
+    @ManyToOne(() => MVocabulary, (voc) => voc.id)
+    public vocId: number;
+
+    @Index()
+    @Column()
+    public sense: string;
 
     @CreateDateColumn()
     public created: number;
@@ -106,20 +97,90 @@ export class Meaning {
 }
 
 @Entity()
-export class Example {
+export class MPronunciation {
     @PrimaryGeneratedColumn()
     public id: number;
 
     @Index()
     @Column()
-    public voc: string;
+    public ipa: string;
 
     @Index()
     @Column()
-    public meanId: number;
+    @ManyToOne(() => MPartOfSpeech, (pos) => pos.id)
+    public posId: number;
+
+    @Index()
+    @Column()
+    public audio: Buffer;
+
+    @CreateDateColumn()
+    public created: number;
+
+    @UpdateDateColumn()
+    public updated: number;
+}
+
+
+@Entity()
+export class MSenseDefinition {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Index()
+    @Column({ default: null, })
+    @ManyToOne(() => MVocabulary, (voc) => voc.id)
+    public vocId: number;
 
     @Column()
-    public example: string;
+    public defininition: string;
+
+    // @Index()
+    // @Column()
+    // public level: Level;
+
+    @CreateDateColumn()
+    public created: number;
+
+    @UpdateDateColumn()
+    public updated: number;
+}
+
+@Entity()
+export class MPhrase {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Index()
+    @Column()
+    @ManyToOne(() => MSense, (sense) => sense.id)
+    public senseId: number;
+
+    @Index()
+    @Column()
+    public phrase: string;
+
+    @CreateDateColumn()
+    public created: number;
+
+    @UpdateDateColumn()
+    public updated: number;
+}
+
+@Entity()
+export class MExample {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Index()
+    @Column()
+    @ManyToOne(() => MVocabulary, (voc) => voc.id)
+    public vocId: number;
+
+    @Index()
+    @Column()
+    @ManyToOne(() => MSenseDefinition, (def) => def.id)
+    public defId: number;
 
     @CreateDateColumn()
     public created: number;
---
+++ src/underlie/cambridge/index.ts
@@ -0,0 +1,10 @@
+export enum CambridgeDicCode {
+    'British' = 'british',
+    'EssentialBritishEnglish' = 'essential-british-english',
+    'EssentialAmericanEnglish' = 'essential-american-english',
+    'AmericanEnglish' = 'american-english',
+    'BusinessEnglish' = 'business-english',
+    'LearnerEnglish' = 'lerner-english',
+}
+
+async function composeVocabulary(word: string) { }
